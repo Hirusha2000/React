893fda83e84c1158338862ff26571af4d3804600
Hirusha2000 <2000hirushaudara@gmail.com>
2024-02-05 12:38:49 +0530

    new updates

--- src/List.tsx
+++ src/List.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
+
 function List(){
 
 
-const cities=[
+let cities=[
     "Sydney",
     "Melbourne",
     "Brisbane",
@@ -14,14 +16,23 @@ const cities=[
     "Darwin"
 ]
 
+ const [selectedIndex ,setSelectedIndex]=useState<number>()
+
+
 
     return(
         <>
-                  <h1>List</h1>
+ <h1>List</h1>
+
+{cities.length==0 && <h1>No Items in the Array</h1>}
 
 <ul className="list-group">
 {
-cities.map((city,index) => <li className="list-group-item" key={index}>{city}</li>)
+cities.map((city,index) => 
+<li className={selectedIndex===index ? "list-group-item active" :"list-group-item "}
+ key={index} 
+ onClick={()=>setSelectedIndex(index)}
+>{city}</li>)
 }
 </ul>
         </>
